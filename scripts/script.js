05012d48b9628c9ad73b28ca8335ad60a5ea8ce5
Nicolas B <97320296+NickBzy@users.noreply.github.com>
2025-03-10 18:08:05 -0400

    Add color changing effect when mouse hovers above divs

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,18 @@ const widthOfDiv = (1/16) * 100;
 for (let row=0; row<16; row++){
     for(let col=0; col<16; col++){
         const div = document.createElement("div")
-        div.classList.add("test")
+        div.classList.add("grid-item")
         div.style.height = heightOfDiv+'%'
         div.style.width = widthOfDiv+'%'
         grid.appendChild(div);
     }
 }
+
+const allItems = document.querySelectorAll(".grid-item");
+
+allItems.forEach(div => {
+    div.addEventListener("mouseover", function(e){
+        e.target.classList.add("grid-item-hover");
+        e.target.style.backgroundColor = "black";
+    });
+});
