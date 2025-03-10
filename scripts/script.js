15601c462824d24edc7b170ac67e6d1ed172124a
Nicolas B <97320296+NickBzy@users.noreply.github.com>
2025-03-10 17:49:50 -0400

    Create the initial 16x16 grid used to sketch

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+const grid = document.querySelector(".grid")
+const heightOfDiv = (1/16) * 100;
+const widthOfDiv = (1/16) * 100;
+
+for (let row=0; row<16; row++){
+    for(let col=0; col<16; col++){
+        const div = document.createElement("div")
+        div.classList.add("test")
+        div.style.height = heightOfDiv+'%'
+        div.style.width = widthOfDiv+'%'
+        grid.appendChild(div);
+    }
+}
