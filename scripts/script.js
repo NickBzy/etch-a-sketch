bc09d59caffca158484514ff38c463aff68b8d49
Nicolas B <97320296+NickBzy@users.noreply.github.com>
2025-03-11 12:54:59 -0400

    Improve settings of the etch a sketch webpage for more flexibility.
    - Add a toggle for user to change the grid size
    - Add a rainbow mode to draw using the colors of the rainbow
    - Add a show/hide grid lines toggle

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,70 @@
-const grid = document.querySelector(".grid")
-const heightOfDiv = (1/16) * 100;
-const widthOfDiv = (1/16) * 100;
+const grid = document.querySelector(".grid");
+const displayGridSize = document.querySelector("#display-grid-size");
+const gridSize = document.querySelector("#current-grid-size");
+const rainbowToggle = document.querySelector("#rainbow-mode");
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+let colorIndex = 0;
 
-for (let row=0; row<16; row++){
-    for(let col=0; col<16; col++){
-        const div = document.createElement("div")
-        div.classList.add("grid-item")
-        div.style.height = heightOfDiv+'%'
-        div.style.width = widthOfDiv+'%'
-        grid.appendChild(div);
+function createGrid(size) {
+    //clear the grid
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+    const heightOfDiv = (1/gridSize.value) * 100;
+    const widthOfDiv = (1/gridSize.value) * 100;
+    for (let row=0; row<gridSize.value; row++){
+        for(let col=0; col<gridSize.value; col++){
+            const div = document.createElement("div")
+            div.classList.add("grid-item")
+            div.style.height = heightOfDiv+'%'
+            div.style.width = widthOfDiv+'%'
+
+            //Changes color of grid items if user hovers over them
+            div.addEventListener("mouseover", function(e){
+                if (rainbowToggle.checked){
+                    e.target.style.backgroundColor = colors[colorIndex % colors.length];
+                    colorIndex++;
+                } else {
+                    e.target.style.backgroundColor = "black";
+                } 
+            });
+
+            grid.appendChild(div);
+        }
     }
 }
 
-const allItems = document.querySelectorAll(".grid-item");
+createGrid(10);
 
-allItems.forEach(div => {
-    div.addEventListener("mouseover", function(e){
-        e.target.style.backgroundColor = "black";
-    });
+gridSize.addEventListener("input", function() {
+    displayGridSize.innerText = gridSize.value +" x "+gridSize.value;
+    createGrid(gridSize.value);
 });
 
+//Clears the grid from drawing if user presses clear button
 const clearButton = document.querySelector(".reset button");
 clearButton.addEventListener("click", function(e){
+    const allItems = document.querySelectorAll(".grid-item");
     allItems.forEach(div => {
         div.style.backgroundColor = "white"
     })
 })
+
+const toggleGridLines = document.querySelector("#grid-lines");
+toggleGridLines.addEventListener("change", function() {
+    const allItems = document.querySelectorAll(".grid-item");
+    if (toggleGridLines.checked){
+        allItems.forEach(div => {
+            div.style.border = "0px"
+        });
+    } else {
+        allItems.forEach(div => {
+            div.style.borderTop = "1px solid black"
+            div.style.borderLeft = "1px solid black"
+        });
+    }
+    
+});
+
+
+
